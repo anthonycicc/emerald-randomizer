e58453d0e35c5c42edbfb5f2bacfb71e299109b5
AnthonyS <anthonycicc@gmail.com>
2017-07-01 14:26:57 -0400

    Added .gitignore and started rewriting the PokeData class

---
+++ code/EmeraldRandomizer/src/emeraldrandomizer/PokeDataKt.kt
@@ -0,0 +1,14 @@
+package emeraldrandomizer
+
+class PokeDataKt(
+        stats: IntArray = intArrayOf(1,1,1,1,1,1),
+        types: IntArray = intArrayOf(0,0),
+        evs: IntArray = intArrayOf(0,0,0,0,0,0),
+        abilities: IntArray = intArrayOf(1,0),
+        heldItems: IntArray = intArrayOf(0,0),
+        statSwaps: IntArray = intArrayOf(0,1,2,3,4,5,6),
+        TMCompat: IntArray = intArrayOf(0,0,0,0,0,0,0,0),
+        tutorCompat: IntArray = intArrayOf(0,0,0,0)) {
+
+
+}
